a64f68e69217692d50a1a2469a2bb9792b45eeb7
Billy Scott <b.scott5@outlook.com>
2020-09-09 18:25:15 +0100

    Add performance improvements notes and code

---
+++ performance_improvements/comparing_broadcast_vs_normal_joins.py
@@ -0,0 +1,13 @@
+start_time = time.time()
+# Count the number of rows in the normal DataFrame
+normal_count = normal_df.count()
+normal_duration = time.time() - start_time
+
+start_time = time.time()
+# Count the number of rows in the broadcast DataFrame
+broadcast_count = broadcast_df.count()
+broadcast_duration = time.time() - start_time
+
+# Print the counts and the duration of the tests
+print("Normal count:\t\t%d\tduration: %f" % (normal_count, normal_duration))
+print("Broadcast count:\t%d\tduration: %f" % (broadcast_count, broadcast_duration))
---
+++ performance_improvements/normal_joins.py
@@ -0,0 +1,6 @@
+# Join the flights_df and aiports_df DataFrames
+normal_df = flights_df.join(airports_df, \
+    flights_df["Destination Airport"] == airports_df["IATA"] )
+
+# Show the query plan
+normal_df.explain()
